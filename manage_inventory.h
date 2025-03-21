37cdd5716e2528eaddde59ca61d2a61d2da34afa
Syeda Juweria Kamran <juweriakai@gmail.com>
2025-03-21 23:35:26 +0500

    Update manage_inventory.h

--- manage_inventory.h
+++ manage_inventory.h
@@ -1,29 +1,40 @@
 #ifndef MANAGE_INVENTORY_H
 #define MANAGE_INVENTORY_H
+
 #include <QDialog>
 #include <QComboBox>
 #include <QLabel>
 #include <QPushButton>
 #include <QTimer>
+#include <QSqlDatabase>   
+#include <QSqlQuery>      
+#include <QSqlError>
+#include "add_update.h"
 
 namespace Ui {
 class manage_inventory;
 }
+
 class manage_inventory : public QDialog
 {
     Q_OBJECT
+
 public:
     explicit manage_inventory(QWidget *parent = nullptr);
     ~manage_inventory();
+
 private slots:
     void on_pushButton_clicked();
     void on_pushButton_3_clicked();
-    void on_pushButton_2_clicked();
+     void on_pushButton_2_clicked();
     void on_tableWidget_cellActivated(int row, int column);
     void applyFilter();
     void updateBlinkEffect();
+    void loadInventoryData();
+
 protected:
     void resizeEvent(QResizeEvent *event) override;
+
 private:
     Ui::manage_inventory *ui;
     QComboBox *filterComboBox;
@@ -32,5 +43,8 @@ private:
     QLabel *titleLabel;
     QTimer *blinkTimer;
     bool blinkState;
+    add_update *addUpdateWindow;
+
 };
+
 #endif // MANAGE_INVENTORY_H
